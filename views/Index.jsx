7a0613fe8bd5233a6fc4896080b14de9f0d7e519
Andy Riecker <ariecker@gmail.com>
2020-05-25 17:59:39 -0400

    styled

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,13 +5,14 @@ class Index extends React.Component {
     render() {
         return (
             <Layout username={this.props.username}>
-                    
+                <div id="indexTitle">The Music List Index</div>    
                 <ul class="indexUlContainer">
                     {
                     this.props.item.map((item, index) => {
                         return (
                             <li>
-                            <h4><a href={`/list/${item._id}`}>{ item.artist }{item.date}</a><br/></h4>
+                            <h4><a href={`/list/${item._id}`}>{ item.artist }</a></h4>
+                            <h4><a href={`/list/${item._id}`}>{item.date}</a><br/></h4>
 
                             <form action={`/list/${item._id}?_method=DELETE`} method="POST">
                                 <input class="deleteButton" type="submit" value="Delete this item from the list"/>
@@ -21,7 +22,7 @@ class Index extends React.Component {
                         })
                     }
                 </ul>
-                <img src="https://blog.expertrec.com/wp-content/uploads/2019/06/Utorrent-Banner1.png" alt="utbanner"/>
+                {/* <img src="https://blog.expertrec.com/wp-content/uploads/2019/06/Utorrent-Banner1.png" alt="utbanner"/> */}
 
             </Layout>
         )
